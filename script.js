e5e587a3099edd67e17188dad9efad558e337d9b
tiram-isu <93144973+tiram-isu@users.noreply.github.com>
2022-04-12 09:01:33 +0200

    JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+// alert("Das ist eine Warnung");
+
+const divElement = document.getElementById("some-element");
+
+console.log(divElement);
+
+divElement.textContent = "Das ist der neue Wert";
+divElement.classList.add("test-klasse");
